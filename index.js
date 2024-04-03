e6a4caca7bc019d0a3b480ccbefe5c6a17c09b94
Shyam Tala <49745830+shyamtala003@users.noreply.github.com>
2024-04-03 10:32:54 +0530

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.post("/", (request, response) => {
+  response.cookie("refresh-token", "sdfnssdfsdfdf", {
+    // httpOnly: true,
+    // secure: true,
+    // sameSite: "none",
+    // maxAge: 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false, // Temporarily for local development
+    sameSite: "lax", // Can use "lax" for local development if not testing cross-site requests
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+  response.json("cookie setted");
+});
+
+app.get("/login", (request, response) => {
+  console.log(request.cookies["refresh-token"]);
+  response.json("cookie received");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
